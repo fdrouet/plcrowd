74e5a4875c0e9b015311020506d512a2bec67283
Frédéric DROUET <dev@tarpoon.org>
2011-08-14 23:26:57 +0200

    Better configuration error managment routes file improvement

--- app/controllers/CrowdSecurity.java
+++ app/controllers/CrowdSecurity.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import net.sf.oval.constraint.NotEmpty;
 import play.Logger;
+import play.Play;
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -31,6 +32,7 @@ import com.atlassian.crowd.service.client.CrowdClient;
  */
 public class CrowdSecurity extends Controller {
     private static final String CROWD_TOKEN = "crowd-sso-token";
+    private static final String CONF_CROWD_PROPERTY = "plcrowd.crowd.properties";
     private static CrowdClient crowdClient;
     private static Object lock = new Object();
 
@@ -38,10 +40,23 @@ public class CrowdSecurity extends Controller {
     static void checkAuthenticated() {
         synchronized (lock) {
             if (crowdClient == null) {
-                ClientProperties clientProperties = ClientPropertiesImpl
-                        .newInstanceFromResourceLocator(new ClientResourceLocator("crowd.properties"));
+                String crowdPropertiesFile = Play.configuration.getProperty(CONF_CROWD_PROPERTY);
+                if (crowdPropertiesFile == null) {
+                    error(500, "The " + CONF_CROWD_PROPERTY + " property must by defined in your application.conf");
+                }
+                ClientResourceLocator crl = new ClientResourceLocator(crowdPropertiesFile);
+                if (crl.getProperties() == null) {
+                    error(500, "The " + crowdPropertiesFile + " can not be found");
+                }
+
+                ClientProperties clientProperties = ClientPropertiesImpl.newInstanceFromResourceLocator(crl);
+
                 RestCrowdClientFactory restCrowdClientFactory = new RestCrowdClientFactory();
-                crowdClient = restCrowdClientFactory.newInstance(clientProperties);
+                try {
+                    crowdClient = restCrowdClientFactory.newInstance(clientProperties);
+                } catch (Exception e) {
+                    error(500, e.getLocalizedMessage());
+                }
             }
         }
 
