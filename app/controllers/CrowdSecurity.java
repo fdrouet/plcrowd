baaef9c130001c04373f729821315127fd536e0b
Frederic DROUET <fdrouet@MacBook-Pro-de-Frederic.local>
2011-08-31 18:43:31 +0200

    new : CrowdSecurity.getUserGroups() to get all the groups of the current logged-in user (the groups are cached for 30 minutes) new : macro #{user.memberOf group:'my-group'} what i want #{/user.memberOf} to execute things only the user is member of the specified group improvement : #{crowd.anonymous} and #{crowd.authenticated} improvement

--- app/controllers/CrowdSecurity.java
+++ app/controllers/CrowdSecurity.java
@@ -4,9 +4,11 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import net.sf.oval.constraint.NotEmpty;
 import play.Logger;
+import play.cache.Cache;
 import play.mvc.Before;
 import play.mvc.Controller;
 import utils.CrowdUtils;
@@ -35,12 +37,14 @@ public abstract class CrowdSecurity extends Controller {
     private static final String CROWD_USER_SSO_TOKEN = "crowd-sso-token";
     private static final String CROWD_USER_LOGIN = "crowd-user-login";
     private static final String CROWD_USER_DISPLAY_NAME = "crowd-user-display-name";
+    private static final String CACHE_USER_GROUPS_KEY_PREFIX = "user-groups-";
 
-    @Before(unless = { "login", "authenticate", "logout", "getCrowdClient" })
+    @Before(unless = { "login", "authenticate", "logout", "getCrowdClient", "getUserGroups", "isAuthenticate" })
     static void checkAuthenticated() {
         CrowdClient crowdClient = CrowdUtils.getCrowdClient();
         flash.put("url", "GET".equals(request.method) ? request.url : "/");
         if (session.contains("crowd-sso-token")) {
+            Logger.info("Check Authentication from token", session.get(CROWD_USER_SSO_TOKEN));
             try {
                 crowdClient.validateSSOAuthentication(session.get(CROWD_USER_SSO_TOKEN), new ArrayList<ValidationFactor>());
                 // redirectToOriginalURL();
@@ -119,7 +123,7 @@ public abstract class CrowdSecurity extends Controller {
     public static void authenticate(@NotEmpty String login, @NotEmpty String password) {
 
         Logger.info("login %s", login);
-        Logger.info("url 2 redirect : %s", flash.get("url"));
+        Logger.debug("url 2 redirect : %s", flash.get("url"));
 
         CrowdClient crowdClient = getCrowdClient();
         UserAuthenticationContext ctx = new UserAuthenticationContext();
@@ -174,6 +178,22 @@ public abstract class CrowdSecurity extends Controller {
     }
 
     /**
+     * Check if the current user is already authenticated (BE CAREFUL : we doesn't ask crowd to check if the current user's sso token is still valid) 
+     * @return
+     */
+    public static boolean isAuthenticated() {
+        return session.contains(CROWD_USER_SSO_TOKEN);
+    }
+    
+    /**
+     * Check if the current user is anonymous (BE CAREFUL : we doesn't ask crowd we only check if the current user has a sso token) 
+     * @return
+     */
+    public static boolean isAnonymous() {
+        return (! session.contains(CROWD_USER_SSO_TOKEN));
+    }
+    
+    /**
      * Get the current logged user login or null if anonymous
      * 
      * @return current user login or null
@@ -209,4 +229,46 @@ public abstract class CrowdSecurity extends Controller {
         redirect(url);
     }
 
+    /**
+     * Get all the groups of the current logged-in user. The groups are loaded
+     * from the cache if available or loaded from Crowd and puted in the cache
+     * 
+     * @return
+     */
+    @SuppressWarnings("finally")
+    public static List<String> getUserGroups() {
+        if (isAuthenticated() == false) {
+            cleanupSession();
+            login();
+        }
+         
+        CrowdClient crowdClient = CrowdUtils.getCrowdClient();
+        List<String> groups = null;
+
+        try {
+            groups = Cache.get(CACHE_USER_GROUPS_KEY_PREFIX + getCurrentUserLogin(), List.class);
+
+            if (groups == null) {
+                groups = crowdClient.getNamesOfGroupsForNestedUser(getCurrentUserLogin(), 0, 100);
+                Cache.safeSet(CACHE_USER_GROUPS_KEY_PREFIX + getCurrentUserLogin(), groups, "30m");
+            }
+        } catch (UserNotFoundException e) {
+            // this case should never happen
+            flash.error("The current user doesn't exists (%s)", e.getLocalizedMessage());
+            Logger.error(e, "The current user doesn't exists (user = %s).", getCurrentUserLogin());
+            cleanupSession();
+            login();
+        } catch (OperationFailedException e) {
+            flash.error("Impossible to fetch the current user's groups ! try again later (%s)", e.getLocalizedMessage());
+            Logger.error(e, "Impossible to fetch the current user's groups (user = %s).", getCurrentUserLogin());
+        } catch (InvalidAuthenticationException e) {
+            flash.error("Impossible to fetch the current user's groups ! (%s)", e.getLocalizedMessage());
+            Logger.error(e, "Impossible to fetch the current user's groups (user = %s).", getCurrentUserLogin());
+        } catch (ApplicationPermissionException e) {
+            flash.error("Impossible to fetch the current user's groups ! (%s)", e.getLocalizedMessage());
+            Logger.error(e, "Impossible to fetch the current user's groups (user = %s).", getCurrentUserLogin());
+        } finally {
+            return groups;
+        }
+    }
 }
